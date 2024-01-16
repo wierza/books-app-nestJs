bc3751692b50f54707971c1e0942f1901a20d890
Dawid <dawid@wierzycki.pl>
2024-01-16 12:45:28 +0100

    Add liked books method

---
+++ src/books/dtos/favourite-book.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class FavouriteBookDTO {
+  @IsNotEmpty()
+  @IsUUID()
+  @IsString()
+  userId: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  @IsString()
+  bookId: string;
+}
